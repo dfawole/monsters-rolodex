e0775c260f2c1d383274fdee0504690baff165c3
OlaFawole <Oladotun.Fawole@eurostar.com>
2019-08-11 20:14:52 +0100

    Updates

---
+++ precache-manifest.cf60c4f96ac67264328d9a947ab6ed29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5981c333590fa24254029c9dad5f01e7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a566a9c3aa9f74a21b1",
+    "url": "/monsters-rolodex/static/css/main.79448d0b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "9a566a9c3aa9f74a21b1",
+    "url": "/monsters-rolodex/static/js/main.71123d1c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
